cf7795a631224d1e0858ca1fa54ac1dae49f1031
JorigeSujitha <jorigesuijtha@gmail.com>
2024-06-25 00:39:50 +0530

    third commit connecting to server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const db = require("./db");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 app.get("/" , (req , res) => {
     console.log("Welcome to our hotel , here is the menu card!");
 })
@@ -14,7 +15,7 @@ app.use("/Person" , personRoutes)
 const MenuItemRoutes = require("./Routes/MenuItemsRoutes");
 app.use("/menu" , MenuItemRoutes);
 
-app.listen(3000 , () => {
+app.listen(PORT, () => {
     console.log("server is listening on port 3000");
 })
 
